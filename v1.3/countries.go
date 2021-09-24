2456895b8b1b507f5445719fbc4dfbb261f85ea4
John Wang <johncwang@gmail.com>
2021-09-24 04:21:29 -0700

    feat: countries: add functions

--- v1.3/countries.go
+++ v1.3/countries.go
@@ -1,5 +1,11 @@
 package randomuser // import "github.com/grokify/go-randomuser/v1.3"
 
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
 const (
 	CountryAustralia     = "AU"
 	CountryBrazil        = "BR"
@@ -19,3 +25,40 @@ const (
 	CountryUnitedKingdom = "GB"
 	CountryUnitedStates  = "US"
 )
+
+func RandomCountry() string {
+	countries := Countries()
+	rand.Seed(time.Now().Unix())
+	return countries[rand.Intn(len(countries))]
+}
+
+func Countries() []string {
+	return []string{
+		CountryAustralia,
+		CountryBrazil,
+		CountryCanada,
+		CountryDenmark,
+		CountryFinland,
+		CountryFrance,
+		CountryGermany,
+		CountryIran,
+		CountryIreland,
+		CountryNetherlands,
+		CountryNewZealand,
+		CountryNorway,
+		CountrySpain,
+		CountrySwitzerland,
+		CountryTurkey,
+		CountryUnitedKingdom,
+		CountryUnitedStates}
+}
+
+func IsCountry(c string) bool {
+	c = strings.ToUpper(c)
+	for _, try := range Countries() {
+		if try == c {
+			return true
+		}
+	}
+	return false
+}
